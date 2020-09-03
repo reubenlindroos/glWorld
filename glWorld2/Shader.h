1747730a1ce993a8fb5982b999cd9a0552aaa6e9
rjl67 <rjl67@bath.ac.uk>
2020-09-03 11:52:46 +0100

    adding shader class

---
+++ glWorld2/Shader.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <glad/glad.h> // include glad to get the required OpenGL headers
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+/**
+ * Encapsulates the shader program setup and checks for bugs on compiling.
+ */
+class Shader
+{
+public:
+	// the program ID
+	unsigned int ID;
+	// constructor reads and builds the shader
+	Shader(const char* vertexPath, const char* fragmentPath);
+};
+
